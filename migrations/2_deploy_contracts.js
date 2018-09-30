fe4d1e201d01ffefcf53aee99847816e7335ff44
edwardgoode <edward.s.goode@vanderbilt.edu>
2018-09-29 19:37:16 -0700

    migrations working

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
 const InsuranceCoin = artifacts.require("./InsuranceCoin.sol")
+const InsuredEvent = artifacts.require("./InsuredEvent.sol")
 
 module.exports = function(deployer) {
-	deployer.deploy(InsuranceCoin, "name1", "nm", 13, 20);
+	deployer.deploy(InsuredEvent, "string _title", "string _description", "string _symbol", 20, 20, ['0xa1aae54e99202578fe60850f6c33616c81e67cc1']);
 };
