32c3fd2b9d1e3aef013126510247a2a27a1a9494
edwardgoode <edward.s.goode@vanderbilt.edu>
2018-10-10 23:16:49 -0700

    updates

--- query.js
+++ query.js
@@ -1,6 +1,15 @@
-module.exports = function() {
-  
-  let adr = web3.eth.accounts[0]
-  
-  console.log('account1: '+adr.toString());
-}
+const artifacts = require('./build/contracts/InsuredEvent.json')
+var Web3 = require("web3")
+var provider = new Web3.providers.HttpProvider("http://localhost:9545")
+const contract = require('truffle-contract')
+
+let InsuredEvent = contract(artifacts);
+
+InsuredEvent.setProvider(provider);
+
+InsuredEvent.deployed().then(function(instance){
+	meta = instance; 
+	return meta.totalSupply()
+}).catch(function () {
+	console.log("caught");
+});
